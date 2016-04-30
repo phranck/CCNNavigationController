ff77f58c255eec0ffa827f34e5712b59ad145cae
Frank Gregor <phranck@cocoanaut.com>
2016-05-01 01:49:42 +0200

    added navigation controller's delegate calls and notifications

--- Example/CCNNavigationController Example/AppDelegate.m
+++ Example/CCNNavigationController Example/AppDelegate.m
@@ -5,9 +5,10 @@
 
 #import "AppDelegate.h"
 #import "TestVC.h"
+#import "CCNNavigationController.h"
 
 
-@interface AppDelegate ()
+@interface AppDelegate () <CCNNavigationControllerDelegate>
 @property (weak) IBOutlet NSWindow *window;
 @end
 
@@ -22,12 +23,52 @@
     
     TestVC *rootVC = [[TestVC alloc] initWithNibName:@"TestVC" bundle:nil];
     CCNNavigationController *navigationController = [[CCNNavigationController alloc] initWithRootViewController:rootVC];
+    navigationController.delegate = self;
+    navigationController.configuration.transitionStyle = CCNNavigationControllerTransitionStyleStack;
+//    navigationController.configuration.transition = CCNNavigationControllerTransitionToUp;
     self.window.contentViewController = navigationController;
-
+    
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserverForName:CCNNavigationControllerWillShowViewControllerNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        NSViewController *viewController = note.userInfo[CCNNavigationControllerNotificationUserInfoKey];
+        NSLog(@"[NOTIFICATION] CCNNavigationControllerWillShowViewControllerNotification: %@", viewController);
+    }];
+    [nc addObserverForName:CCNNavigationControllerDidShowViewControllerNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        NSViewController *viewController = note.userInfo[CCNNavigationControllerNotificationUserInfoKey];
+        NSLog(@"[NOTIFICATION] CCNNavigationControllerDidShowViewControllerNotification: %@", viewController);
+    }];
+    [nc addObserverForName:CCNNavigationControllerWillPopViewControllerNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        NSViewController *viewController = note.userInfo[CCNNavigationControllerNotificationUserInfoKey];
+        NSLog(@"[NOTIFICATION] CCNNavigationControllerWillPopViewControllerNotification: %@", viewController);
+    }];
+    [nc addObserverForName:CCNNavigationControllerDidPopViewControllerNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        NSViewController *viewController = note.userInfo[CCNNavigationControllerNotificationUserInfoKey];
+        NSLog(@"[NOTIFICATION] CCNNavigationControllerDidPopViewControllerNotification: %@", viewController);
+    }];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+#pragma mrk - CCNNavigationControllerDelegate
+
+- (void)navigationController:(CCNNavigationController *)navigationController willShowViewController:(__kindof NSViewController *)viewController animated:(BOOL)animated {
+    NSLog(@"[DELEGATE] navigationController: willShowViewController: %@", viewController);
+}
+
+- (void)navigationController:(CCNNavigationController *)navigationController didShowViewController:(__kindof NSViewController *)viewController animated:(BOOL)animated {
+    NSLog(@"[DELEGATE] navigationController: didShowViewController: %@", viewController);
+}
+
+- (void)navigationController:(CCNNavigationController *)navigationController willPopViewController:(__kindof NSViewController *)viewController animated:(BOOL)animated {
+    NSLog(@"[DELEGATE] navigationController: willPopViewController: %@", viewController);
+}
+
+- (void)navigationController:(CCNNavigationController *)navigationController didPopViewController:(__kindof NSViewController *)viewController animated:(BOOL)animated {
+    NSLog(@"[DELEGATE] navigationController: didPopViewController: %@", viewController);
+}
+
 
 @end
